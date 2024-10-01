5201fa191d06ab5df95e3c604350313f0236d333
Baran <me@baran.wang>
2024-10-01 11:57:18 +0000

    chore: commit code
    codespace 太难用了

--- packages/arguments-builder/src/bin.ts
+++ packages/arguments-builder/src/bin.ts
@@ -42,23 +42,25 @@ program
   .command('build')
   .option('-c --config <config>', 'config file')
   .action(async (option) => {
-    let config: {default: ArgumentsBuilderConfig}
-    if (option.config && fs.existsSync(option.config)) {
-      config = await bundleRequire(option.config);
-    } else {
-      config = await Promise.race(
-        ['arguments-builder.config.js', 'arguments-builder.config.mjs', 'arguments-builder.config.ts'].map(
-          async (file) => bundleRequire(file),
-        ),
-      );
-    }
-    if (!config?.default) {
+    const configPath = [
+      option.config,
+      'arguments-builder.config.js',
+      'arguments-builder.config.mjs',
+      'arguments-builder.config.ts',
+    ].find((item) => fs.existsSync(item));
+
+    if (!configPath) {
       logger.error('未找到配置文件');
       return;
     }
-    const resp= argumentsBuilderConfigSchema.safeParse(config.default)
-    
-    console.log(resp)
+    const config = await bundleRequire(configPath);
+    const resp = argumentsBuilderConfigSchema.safeParse(config.default);
+
+    if (!resp.success) {
+      logger.error('配置文件有误', resp)
+      return
+    }
+    console.log(resp);
   });
 
 program.parse(process.argv);
