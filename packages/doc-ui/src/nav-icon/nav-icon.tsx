52a06d0879caf0ca6dcbe2e552bae77126bd4dec
baranwang <me@baran.wang>
2024-10-16 10:53:13 +0800

    feat: 更新 doc-ui 样式

--- packages/doc-ui/src/nav-icon/nav-icon.tsx
+++ packages/doc-ui/src/nav-icon/nav-icon.tsx
@@ -1,11 +1,11 @@
 import { Popover } from '../popover';
-import IconNav from './icon.svg';
+import IconNav from './assets/icon.svg';
 import styles from './nav-icon.module.scss';
 import { PopoverContent } from './popover-content';
 
 export const NavIcon: React.FC = () => {
   return (
-    <Popover overlay={<PopoverContent />} placement="bottomLeft">
+    <Popover overlay={<PopoverContent />} placement='bottomLeft'>
       <span className={styles.icon}>
         <IconNav />
       </span>
--- packages/doc-ui/src/nav-icon/popover-content.tsx
+++ packages/doc-ui/src/nav-icon/popover-content.tsx
@@ -2,19 +2,19 @@ import styles from './popover-content.module.scss';
 
 const PROJECTS = [
   {
-    icon: '',
+    icon: 'https://avatars.githubusercontent.com/u/2111377?s=80&v=4',
     url: 'https://NSRingo.github.io/',
     title: 'iRingo',
     description: '解锁完整的 Apple 功能和集成服务',
   },
   {
-    icon: '🍿️',
+    icon: 'https://avatars.githubusercontent.com/u/100578089?s=80&v=4',
     url: 'https://DualSubs.github.io/',
     title: 'DualSubs',
     description: '双语及增强字幕生成工具',
   },
   {
-    icon: '🪐',
+    icon: 'https://avatars.githubusercontent.com/u/129515498?s=80&v=4',
     url: 'https://BiliUniverse.io/',
     title: 'BiliUniverse',
     description: '哔哩哔哩功能优化及增强解决方案',
@@ -24,8 +24,8 @@ const PROJECTS = [
 export const PopoverContent: React.FC = () => {
   const renderLink = ({ icon, url, title, description }: (typeof PROJECTS)[number]) => {
     return (
-      <a key={title} className={styles.item} href={url} target="_blank" rel="noopener noreferrer">
-        <span className={styles.icon}>{icon}</span>
+      <a key={title} className={styles.item} href={url} target='_blank' rel='noopener noreferrer'>
+        <img className={styles.icon} src={icon} alt={title} />
         <div>
           <span className={styles.title}>{title}</span>
           <span className={styles.description}>{description}</span>
