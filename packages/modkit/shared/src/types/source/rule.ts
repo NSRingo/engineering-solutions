8b6f4842d47ae88b62584de28e1c67a1999ef0f8
Virgil Clyne <Virgil@nanocat.me>
2024-10-21 02:38:34 +0800

    feat: rule type
    Update rule.ts

--- packages/modkit/shared/src/types/source/rule.ts
+++ packages/modkit/shared/src/types/source/rule.ts
@@ -1,17 +1,56 @@
-export interface RuleSetRule {
-  type: 'RULE-SET';
+export type RuleType =
+  | 'DOMAIN'
+  | 'DOMAIN-SUFFIX'
+  | 'DOMAIN-KEYWORD'
+  | 'DOMAIN-SET'
+  | 'IP-CIDR'
+  | 'IP-CIDR6'
+  | 'GEOIP'
+  | 'IP-ASN'
+  | 'USER-AGENT'
+  | 'URL-REGEX'
+  | 'PROCESS-NAME'
+  | 'AND'
+  | 'OR'
+  | 'NOT'
+  | 'SUBNET'
+  | 'DEST-PORT'
+  | 'IN-PORT'
+  | 'SRC-PORT'
+  | 'SRC-IP'
+  | 'PROTOCOL'
+  | 'SCRIPT'
+  | 'CELLULAR-RADIO'
+  | 'DEVICE-NAME'
+  | 'RULE-SET'
+  | 'FINAL';
+
+export type policyNameType =
+  | 'DIRECT'
+  | 'REJECT'
+  | 'REJECT-NO-DROP'
+  | 'REJECT-TINYGIF'
+  | 'CELLULAR'
+  | 'CELLULAR-ONLY'
+  | 'HYBRID'
+  | 'NO-HYBRID'
+  | string;
+
+export interface Rule {
+  type: RuleType;
+  content?: string;
   /**
    * 对应 `source.assets` 中的 key
    */
-  assetKey: string;
+  assetKey?: string;
   /**
    * 策略名
    */
-  policyName?: string;
+  policyName?: policyNameType;
   /**
    * 描述
    */
   description?: string;
 }
 
-export type ModuleRule = string | RuleSetRule;
+export type ModuleRule = string | Rule;
