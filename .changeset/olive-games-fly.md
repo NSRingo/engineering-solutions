6a82c6fe0f89fc006795b6d1ecdd44aae3c6980d
baranwang <me@baran.wang>
2024-11-10 23:27:09 +0800

    feat: 增加 ModuleContent 导出

---
+++ .changeset/olive-games-fly.md
@@ -0,0 +1,5 @@
+---
+"@iringo/modkit": patch
+---
+
+增加 ModuleContent 导出
---
+++ packages/modkit/core/src/content.ts
@@ -0,0 +1,5 @@
+import type { ModuleContent } from '@iringo/modkit-shared';
+
+export type { ModuleContent };
+
+export const defineModuleContent = (content: ModuleContent) => content;
